cfd8165b939256ea94fb07b1cf84e627380b0525
ghatikawaykole <ghatika99@gmail.com>
2021-09-09 00:51:32 +0530

    Even and odd number compute

--- src/com/example/basicProgram/basicProgram.java
+++ src/com/example/basicProgram/basicProgram.java
@@ -3,23 +3,14 @@ import java.util.Scanner;
 
 public class basicProgram {
     public static void main(String args[]) {
-        // Swap the number
-        int num, num1, result=0;
-        int a=1;
-        int c=0;
+        // Number is even and odd program
+        int num;
         Scanner in = new Scanner(System.in);
         System.out.println("Enter the number");
-        System.out.println("Enter the first number");
         num = in.nextInt();
-        System.out.println("Enter the second number");
-        num1 = in.nextInt();
-        c = num;
-        num =num1;
-        num1 = c;
-        System.out.println("Swaping with third varable first number"+num+"second number"+num1);
-        num = num + num1;
-        num1 = num - num1;
-        num = num - num1;
-        System.out.println("Number swap first number"+num+"second number"+num1);
+         if(num %2 ==0)
+             System.out.println("Number is even");
+         else
+             System.out.println("Number is odd");
     }
 }
