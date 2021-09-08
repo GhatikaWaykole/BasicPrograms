b72bca4e0fcc471a2a8a8d01bb52d4ecf44e53e0
ghatikawaykole <ghatika99@gmail.com>
2021-09-08 23:48:58 +0530

    Basic Program practice

---
+++ src/com/example/basicProgram/basicProgram.java
@@ -0,0 +1,8 @@
+package com.example.basicProgram;
+import java.util.Scanner;
+
+public class basicProgram {
+    public  static void main(String args[]){
+        System.out.println("Welcome to basic programs");
+    }
+}
