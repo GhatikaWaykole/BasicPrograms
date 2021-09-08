a91c5ab59a46fcf3bd0bd0e64f61cd371ba2424e
ghatikawaykole <ghatika99@gmail.com>
2021-09-09 00:00:56 +0530

    Leap year compute program

--- src/com/example/basicProgram/basicProgram.java
+++ src/com/example/basicProgram/basicProgram.java
@@ -3,22 +3,13 @@ import java.util.Scanner;
 
 public class basicProgram {
     public static void main(String args[]) {
-        // Flip coin and print percentage of Head and tails
-        int tail=0, head=0;
+        // Program to compute the lead year
+        int year;
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter the number of times to flip the coin");
-        int num  = in.nextInt();
-        //System.out.println("number is"+num1);
-        for(int i=0;i<num;i++){
-            int num1 = (int) Math.floor(Math.random() * 2);
-            if(num1 < 0.5)
-                tail +=1;
-            else
-                head +=1;
-
-        }
-        int headPercentage = head * 100 / num;
-        int tailPercentage = tail * 100 / num;
-        System.out.println("Percentage of head"+headPercentage+"percentage of tail"+tailPercentage);
+        year = in.nextInt();
+        if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
+            System.out.println("Given year is leap year");
+        else
+            System.out.println("Given year is not the leap year");
     }
 }
