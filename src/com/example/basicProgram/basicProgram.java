bfe28b977bb65b16b35502410314853ef4e088cd
ghatikawaykole <ghatika99@gmail.com>
2021-09-09 00:46:30 +0530

    Swap the number in two ways

--- src/com/example/basicProgram/basicProgram.java
+++ src/com/example/basicProgram/basicProgram.java
@@ -3,17 +3,23 @@ import java.util.Scanner;
 
 public class basicProgram {
     public static void main(String args[]) {
-        // Quotient and remainder program
+        // Swap the number
         int num, num1, result=0;
         int a=1;
+        int c=0;
         Scanner in = new Scanner(System.in);
         System.out.println("Enter the number");
         System.out.println("Enter the first number");
         num = in.nextInt();
         System.out.println("Enter the second number");
         num1 = in.nextInt();
-        int quotient = num / num1;
-        int remainder = num % num1;
-        System.out.println("quotient of numbers"+quotient+"remainder of the numbers"+remainder);
+        c = num;
+        num =num1;
+        num1 = c;
+        System.out.println("Swaping with third varable first number"+num+"second number"+num1);
+        num = num + num1;
+        num1 = num - num1;
+        num = num - num1;
+        System.out.println("Number swap first number"+num+"second number"+num1);
     }
 }
