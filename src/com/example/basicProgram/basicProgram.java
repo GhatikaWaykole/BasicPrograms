b823d23c0abd5642cb13b57ea7c50624995e5f15
ghatikawaykole <ghatika99@gmail.com>
2021-09-10 20:30:16 +0530

    Compute the sum of three number is zero and display the number

--- src/com/example/basicProgram/basicProgram.java
+++ src/com/example/basicProgram/basicProgram.java
@@ -34,6 +34,20 @@ public class basicProgram {
             for(int j=0;j<array.length;j++) {
                 System.out.println("Elemets in array"+array[i][j]);
             }
+            //sum of three interger add to zero
+            int[] array1 = {1,0,-1,2,0,-2};
+            int n1= array1.length;
+            for(i = 0; i<n1-2; i++){
+                int a= array1[i];
+                for(int j=i+1;j<n1-1;j++)
+                {
+                    int b= array1[j];
+                    for(int k=j+1;k<n1;k++){
+                        int c = array1[k];
+                        if(array1[i]+array1[j]+array1[k] == 0)
+                            System.out.println("The number having sum zero "+a+" "+b+" "+c);                    }
+                }
+            }
         }
     }
 }
